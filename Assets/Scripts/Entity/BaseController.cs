abe90dde311c4ae1e211ceec86bf3c44ab823d2f
박원희 <spilfive3@gmail.com>
2025-05-05 18:44:45 +0900

    [Feat] Player Move, Rotate
    플레이어의 이동과 회전을 구현했습니다.

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -6,8 +6,41 @@ public class BaseController : MonoBehaviour
 {
     protected AnimationHandler animationHandler;
 
+
+    public float MoveSpeed = 5f;
     protected virtual void Awake()
     {
         animationHandler = GetComponent<AnimationHandler>();
+    }
+
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDirection = new Vector2(horizontal, vertical).normalized;
+
+        transform.position += (Vector3)(moveDirection * MoveSpeed * Time.deltaTime);
+
+        animationHandler.Move(moveDirection);
+        Rotate(moveDirection);
+    }
+
+
+    public void Rotate(Vector2 obj)
+    {
+        if (obj.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (obj.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class PlayerController : BaseController
 {
-    public float MoveSpeed = 5f;
-
-    void Update()
-    {
-        OnMove();
-    }
-
-    public void OnMove()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector2 moveDirection = new Vector2(horizontal, vertical).normalized;
-
-        transform.position += (Vector3)(moveDirection * MoveSpeed * Time.deltaTime);
-
-        animationHandler.Move(moveDirection);
-        
-    }
+    
 }
